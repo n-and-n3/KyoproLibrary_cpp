0786dd61aabd607cf221e1be190eb27b1cc89264
n3 <alphagameplayerpro@gmail.com>
2025-11-12 19:02:09 +0900

    UnionFindの実装

---
+++ UnionFind.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+struct UnionFind{
+    vector<int> parent;
+    UnionFind(int n) : parent(n,-1){ }
+
+    int root(int x){
+        if (parent[x] < 0) {
+            return x;
+        } else {
+            parent[x] = root(parent[x]);
+            return parent[x];
+        }
+    }
+
+    int same(int x,int y){
+        return root(x) == root(y);
+    }
+
+    int size(int x){
+        return -parent[root(x)];
+    }
+
+    bool marge(int x,int y){
+        int xr = root(x),yr = root(y);
+        if (xr == yr){
+            return false;
+        }
+        if (parent[xr] <= parent[yr]){
+            parent[xr] += parent[yr];
+            parent[yr] = xr;
+        } else {
+            parent[yr] += parent[xr];
+            parent[xr] = yr;
+        }
+        return true;
+    }
+
+    void print(){
+      cout << "{";
+        for (int i = 0; i < parent.size(); i++){
+            cout << parent[i] << ", ";
+        }
+      cout << "}" << "\n";
+    }
+};
